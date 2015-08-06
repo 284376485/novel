732e5f3cf68d81c0d12b38ec6a8a8e896a1beb61
284376485 <284376485@qq.com>
2015-08-06 13:11:45 +0800

    1

---
+++ basic/controllers/SearchController.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\controllers;
+use yii\web\controller;
+use app\models\bookinfo;
+
+class SearchController extends controller{
+	public $enableCsrfValidation = false;
+	public function actionIndex(){
+		$keywords = $_GET['keyword'];
+		$array = bookinfo::find()->andFilterWhere(['like','bookname',$keywords])
+								->orwhere(['like','author',$keywords])
+								->orwhere(['like','bookname_pinyin',$keywords])
+								->asArray()
+								->all();
+		//var_dump($array);exit;
+		//$this->redirect('http://www.baidu.com'); 可跳转
+		return $this->render('index',[
+					'keywords' => $keywords,
+					'search_result' => $array,
+				]);
+	} 
+}
+
+?>
